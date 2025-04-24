440ba6554f31c0990f0b53cb8d0f0d7327c4cb5c
nisarg1133 <nisargtyu123@gmail.com>
2025-04-24 14:36:24 +0530

    Add files via upload

---
+++ ll.py
@@ -0,0 +1,5 @@
+a=input("enter your first name ")
+b=input("enter your first name ")
+
+c=(f"hello,{a+b}! welcome to the python progam ")
+print(c)
---
+++ nisq.py
@@ -0,0 +1,11 @@
+a=int(input("enter a no "))
+b=int(input("enter a no "))
+
+c=a+b
+d=a-b
+e=a*b
+F=a/b
+print(f"additon:{c}")
+print(f"multiplication:{e}")
+print(f"subtraction:{d}")
+print(f"division:{F}")
